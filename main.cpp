b6848ae653936d8f705c670e7aa932adf8ff2a58
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-21 11:44:46 -0800

    Second main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,22 @@
-#include "MaxHeap.h"
 #include <iostream> 
-#include <fstream> 
+#include "MaxHeap.h"
 
 int main() { 
     MaxHeap heap; 
-    std::string filename; 
-    std::cout << "Enter file name, or leave it empty for manually entering numbers: "; 
-    std::getline(std::cin, filename); 
-    if (!filename.empty()) { 
-        std::ifstream file(filename); 
-        if (!file.is_orpen()) { 
-            std::cerr << "Cant open file silly"; 
-            return 1; 
-        }
-        int num; 
-        while (file >> num) { 
-            heap.insert(num); 
-        }
-        file.close(); 
-    } else { 
-        std::cout << "Enter a number between 1 and 100. (Type -1 to quit)"; 
-        int num;
-        while(std::cin >> num && num != -1) { 
-            heap.inset(num); 
-        }
+    
+    heap.insert(10); 
+    heap.insert(20); 
+    heap.insert(15); 
+    heap.insert(30); 
+    std::cout << "Heap: " << std::endl; 
+    heap.display(); 
+
+    std::cout << "Removing the elements: " << std::endl; 
+    while(heap.getSize() > 0); { 
+        std::cout << "Removed: " << heap.remove() << std::endl; 
     }
 
-        std::cout << "Heap: " << std::endl; 
-        heap.display(); 
-        std::cout << "Removing the elements one at a time: " << sdt::endl;
-        while (heap.remove() != -1); 
-        std::cout << "Your heap after removing: " << std::endl; 
-        heap.display(); 
-        std::cout << "Removing all elements: " << std::endl; 
-        heap.removeAll(); 
-
-        return 0; 
+    return 0; 
 
 }
+
