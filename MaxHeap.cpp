54f5d77e63278e72f6ffc7e16828d8c2d9a72f96
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-21 11:44:07 -0800

    Second version MaxHeap.cpp

--- MaxHeap.cpp
+++ MaxHeap.cpp
@@ -1,80 +1,75 @@
-#include <iostream> 
-#include <cmath> 
 #include "MaxHeap.h"
-#define MAX_SIZE 100; 
 
-MaxHeap::MaxHeap() : size(0) {} 
+MaxHeap::MaxHeap() : heapSize(0) {}
 
-void MaxHeap::heapifyUp(int index) { 
+void MaxHeap::heapifyUp(int index) {
     if (index <= 1) return; 
     int parent = index / 2;
-    if (heap[index] > heap[parent]) { 
-        std::swap(heap[index], heap[parent]); 
-        heapifyUp(parent); 
+    if (heap[index] > heap[parent]) {
+        std::swap(heap[index], heap[parent]);
+        heapifyUp(parent);
     }
 }
 
-void MaxHeap::heapifyDown(int index) { 
-    int leftChild = index * 2; 
-    int rightChild = index * 2 + 1; 
-    int largest = index; 
+void MaxHeap::heapifyDown(int index) {
+    int leftChild = index * 2;
+    int rightChild = index * 2 + 1;
+    int largest = index;
 
-    if (leftChild <= size && heap[leftChild] > heap[largest]) { 
+    if (leftChild <= heapSize && heap[leftChild] > heap[largest])
         largest = leftChild;
-    }
+    if (rightChild <= heapSize && heap[rightChild] > heap[largest])
+        largest = rightChild;
 
-    if (rightChild <= size && heap[rightChild] > heap[largest]) { 
-        largest = rightChild; 
-    }
-
-    if (largest != index) { 
-        std::swap(heap[index], heap[largest]); 
-        heapifyDown(largest); 
+    if (largest != index) {
+        std::swap(heap[index], heap[largest]);
+        heapifyDown(largest);
     }
 }
 
-void MaxHeap::insert (int value) { 
-    if (size >= MAX_SIZE) { 
-        std::cerr << "Heap is at capacity :) \n" << std::endl;
-        return; 
+void MaxHeap::insert(int value) {
+    if (heapSize >= MAX_SIZE) {
+        std::cerr << "Heap is full!" << std::endl;
+        return;
     }
-
-    heap[++size] = value; 
-    heapifyUp(size);
+    heap[++heapSize] = value;
+    heapifyUp(heapSize);
 }
 
-int MaxHeap::remove() { 
-    if (size <= 0) { 
-        std::cerr << "Heap is completely empty :(\n";
+int MaxHeap::remove() {
+    if (heapSize <= 0) {
+        std::cerr << "Heap is empty!" << std::endl;
         return -1;
     }
-    
-    int root = heap[1]; 
-    heap[1] = heap[size=--];
-    heapifyDown(1); 
-    return root; 
-
+    int root = heap[1];
+    heap[1] = heap[heapSize--];
+    heapifyDown(1);
+    return root;
 }
 
-void MaxHeap::removeAll() { 
-    while (size > 0) { 
-        std::cout << remove() << std::endl; 
+int MaxHeap::removeAll() {
+    int removedCount = 0;
+    while (heapSize > 0) {
+        std::cout << remove() << std::endl;
+        removedCount++;
     }
+    return removedCount;
 }
 
-void MaxHeap::displau() { 
-    int levels = log2(size) + 1; 
-    int currLevelNodes = 1; 
-    int currLevelCount = 0; 
+void MaxHeap::display() {
+    int levels = log2(heapSize) + 1;
+    int currLevelNodes = 1;
+    int currLevelCount = 0;
 
-    for (int i = 1; i <= size; i++) { 
-        std::cout << heap[i] << " "; 
-        currLevelCount++; 
+    for (int i = 1; i <= heapSize; i++) {
+        std::cout << heap[i] << " ";
 
+        currLevelCount++;
         if (currLevelCount == currLevelNodes) {
-            std::cout << std::endl; 
-            currLEvelCount = 0; 
-            currLevelNodes *= 2; 
-         }
+            std::cout << std::endl;
+            currLevelCount = 0;
+            currLevelNodes *= 2;
+        }
     }
 }
+
