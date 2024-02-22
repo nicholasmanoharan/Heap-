4f02755ba9774a5931da3c1ea3a0a4c4672927d1
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-22 10:42:39 -0800

    I don't even know what main.cpp update this is

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
-#include <iostream> 
+#include <iostream>
 #include "MaxHeap.h"
 
-int main() { 
-    MaxHeap heap; 
-    
-    heap.insert(10); 
-    heap.insert(20); 
-    heap.insert(15); 
+int main() {
+    MaxHeap heap;
+
+
+    heap.insert(10);
+    heap.insert(20);
+    heap.insert(15);
     heap.insert(30); 
-    std::cout << "Heap: " << std::endl; 
-    heap.display(); 
-
-    std::cout << "Removing the elements: " << std::endl; 
-    while(heap.getSize() > 0); { 
-        std::cout << "Removed: " << heap.remove() << std::endl; 
+    std::cout << "Heap:" << std::endl;
+    heap.display();
+    
+    std::cout << "Removing elements:" << std::endl;
+    while (heap.getSize() > 0) { 
+        std::cout << "Removed: " << heap.remove() << std::endl;
     }
-
-    return 0; 
-
+    
+    return 0;
 }
-
