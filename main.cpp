820695c90ca7dcb70e8cd5e6b1e2d9149eeab53b
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-23 21:13:05 -0800

    New version of main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "MaxHeap.h"
 
 int main() {
     MaxHeap heap;
-
     std::string input;
-    std::cout << "Enter numbers (separated by spaces) or filename: ";
-    std::getline(std::cin, input);
-    std::ifstream file(input);
-    if (file) {
-        heap.loadFromFile(input);
-    } else {
-        std::istringstream iss(input);
-        int num;
-        while (iss >> num) {
-            heap.insert(num);
+
+    do {
+        std::cout << "Enter numbers (separated by spaces) or filename: ";
+        std::getline(std::cin, input);
+        std::ifstream file(input);
+        if (file) {
+            heap.loadFromFile(input);
+        } else {
+            std::istringstream iss(input);
+            int num;
+            while (iss >> num) {
+                heap.insert(num);
+            }
         }
-    }
+
+   
+        std::cout << "Heap:" << std::endl;
+        heap.display();
+        std::cout << "Removing elements:" << std::endl;
+        heap.removeAll();
+        std::cout << "Do you want to use a new set of numbers? (yes/no): ";
+        std::getline(std::cin, input);
+
+    } while (input == "yes");
+        return 0;
+    
 
     
-    std::cout << "Heap:" << std::endl;
-    heap.display();
-    std::cout << "Removing elements:" << std::endl;
-    while (heap.getSize() > 0) { 
-        std::cout << "Removed: " << heap.remove() << std::endl;
-    }
-    
-    return 0;
 }
