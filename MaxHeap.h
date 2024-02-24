bd5f8583e50a94fcb1742e5bd5c7ad6dee3a4f0e
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-23 22:13:51 -0800

    FInal MaxHeap.h

--- MaxHeap.h
+++ MaxHeap.h
@@ -1,5 +1,6 @@
 #ifndef MAXHEAP_H
 #define MAXHEAP_H
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -10,14 +11,18 @@ class MaxHeap {
 
 private:
 
+    //Setting the size for the heap
     static const int MAX_SIZE = 100;
     int heap[MAX_SIZE];
+
+    //Checking the size of the heap
     int heapSize;
     void heapifyUp(int index);
     void heapifyDown(int index);
 
 public:
 
+    //Initializing the heap
     MaxHeap();
     void insert(int value);
     int remove();
@@ -27,4 +32,4 @@ public:
 
 };
 
-#endif 
+#endif
