e61894cf22b5ab5749a742bbe79ed8874dc65488
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-02-23 21:13:38 -0800

    New version MaxHeap.h

--- MaxHeap.h
+++ MaxHeap.h
@@ -1,29 +1,30 @@
-#ifndef MAX_HEAP_H
-#define MAX_HEAP_H
-
-#define MAX_SIZE 100
-
+#ifndef MAXHEAP_H
+#define MAXHEAP_H
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
 #include <cmath>
 
 class MaxHeap {
+
 private:
+
+    static const int MAX_SIZE = 100;
     int heap[MAX_SIZE];
     int heapSize;
     void heapifyUp(int index);
     void heapifyDown(int index);
 
 public:
+
     MaxHeap();
     void insert(int value);
     int remove();
-    int removeAll();
+    void removeAll();
     void display();
-    int getSize() const;
-    void loadFromFile(const std::string& filename);
+    void loadFromFile(const std::string& fileName);
+
 };
 
-#endif
-
+#endif 
